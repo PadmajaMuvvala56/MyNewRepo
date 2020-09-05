54790772785184b903c703f6fb65b06eac77f151
PadmajaMuvvala56 <padmaja.muvvala2020@gmail.com>
2020-09-05 17:41:05 +0530

    My 3rd commit for 2 files

--- LearnJava/src/myFirstJavaProgram/HelloWorld.java
+++ LearnJava/src/myFirstJavaProgram/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World..");
-		System.out.print("This is my first java program");
+		System.out.print("This is my first Java Program");
 	}
 
 }
